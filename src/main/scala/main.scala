46bcd1c6add08f9a0431481f561322fcff422359
Christopher Spears <cspears2002@gmail.com>
2024-12-08 15:03:02 -0800

    Start working on money class
    The subtraction method is broken.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,11 @@
 import fraction.Fraction
+import money.Money
 
 @main
 def main(): Unit =
   println("Hello world!")
-  testFractions()
+  // testFractions()
+  testMoney()
 
 def testFractions(): Unit =
   val product = Fraction(1, 2) * Fraction(1, 2)
@@ -14,4 +16,11 @@ def testFractions(): Unit =
 
   val sum = Fraction(2, 21) + Fraction(1, 6)
   println(s"The answer is $sum.")
+
+def testMoney(): Unit =
+  val sum = Money(1, 75) + Money(0, 50)
+  println(s"The answer is $sum")
+
+  val difference = Money(1, 75) - Money(0, 50)
+  println(s"The answer is $difference")
 
---
+++ src/main/scala/money/Money.scala
@@ -0,0 +1,17 @@
+package money:
+
+  import scala.annotation.targetName
+  
+  class Money(var dollars: Int = 0, var cents: Int = 0):
+    override def toString: String = s"$dollars.$cents"
+    @targetName("add") def+(other: Money): Money =
+      val totalCents = cents + other.cents
+      val totalDollars = dollars + other.dollars
+      val newDollars = totalDollars + (totalCents / 100)
+      val newCents = totalCents % 100
+      Money(newDollars, newCents)
+    @targetName("subtract") def-(other: Money): Money =
+      val totalCents = (dollars * 100) + cents
+      val otherTotalCents = (other.dollars * 100) + cents
+      val diffInCents = totalCents - otherTotalCents
+      Money(diffInCents / 100, diffInCents % 100)
