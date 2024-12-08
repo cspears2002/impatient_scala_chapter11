d7f9634dcf0a4b8644b9d08c63a5271305e62d51
Christopher Spears <cspears2002@gmail.com>
2024-12-08 14:02:35 -0800

    Implement IDE recommendations

--- src/main/scala/fraction/Fraction.scala
+++ src/main/scala/fraction/Fraction.scala
@@ -1,5 +1,5 @@
 package fraction:
-  import scala.annotation.targetName
+  import scala.annotation.{tailrec, targetName}
   import scala.math.abs
   
   class Fraction(n: Int, d: Int):
@@ -7,9 +7,10 @@ package fraction:
     private val den: Int = if d == 0 then 0 else d * sign(d) / gcd(n, d)
 
     override def toString: String = s"$num/$den"
-    def sign(a: Int): Int = if a > 0 then 1 else if a < 0 then -1 else 0
-    def gcd(a: Int, b: Int): Int = if b == 0 then abs(a) else gcd(b, a % b)
-    def lcm(a: Int, b: Int): Int = abs(a * b) / gcd(a, b)
+    private def sign(a: Int): Int = if a > 0 then 1 else if a < 0 then -1 else 0
+    @tailrec
+    private def gcd(a: Int, b: Int): Int = if b == 0 then abs(a) else gcd(b, a % b)
+    private def lcm(a: Int, b: Int): Int = abs(a * b) / gcd(a, b)
 
     @targetName("multiply") def *(other: Fraction) = Fraction(num * other.num, den * other.den)
     @targetName("divide") def /(other: Fraction) = Fraction(num * other.den, den * other.num)
