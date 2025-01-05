5167f9e6e1af9f2a1d24cf355f39a4b42e7d9fbf
Christopher Spears <cspears2002@gmail.com>
2025-01-04 17:09:39 -0800

    Completed HtmlTable and tests

--- src/main/scala/HtmlTable/HtmlTable.scala
+++ src/main/scala/HtmlTable/HtmlTable.scala
@@ -6,11 +6,33 @@ class HtmlTable:
 
   private val table = new ListBuffer[ListBuffer[String]]
 
-  def|(data: String): HtmlTable =
+  def |(data: String): HtmlTable = {
     if (table.isEmpty) {
       table += new ListBuffer[String]
     }
     table.last += data
     this
+  }
+
+  def ||(data: String): HtmlTable = {
+    table += new ListBuffer[String]
+    table.last += data
+    this
+  }
+
+  def toHtml: String = {
+    val sb = new StringBuilder("<table>")
+    for (row <- table) {
+      sb ++= "<tr>"
+      for (cell <- row) {
+        sb ++= "<td>"
+        sb ++= cell
+        sb ++= "</td>"
+      }
+      sb ++= "</tr>"
+    }
+    sb ++= "</table>"
+    sb.toString()
+  }
 
 
--- src/test/scala/HtmlTableTest.scala
+++ src/test/scala/HtmlTableTest.scala
@@ -6,7 +6,7 @@ class HtmlTableTest extends AnyFunSuite:
   test("Table") {
     val table = HtmlTable() | "Java" | "Scala" || "Gosling" | "Odersky" || "JVM" | "JVM, .NET"
     assert {
-      table == "<table><tr><td>Java</td><td>Scala</td></tr>" +
+      table.toHtml == "<table><tr><td>Java</td><td>Scala</td></tr>" +
         "<tr><td>Gosling</td><td>Odersky</td></tr>" +
         "<tr><td>JVM</td><td>JVM, .NET</td></tr></table>"
     }
