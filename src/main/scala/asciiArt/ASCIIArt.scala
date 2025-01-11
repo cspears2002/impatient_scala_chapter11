ad4bf9a7f39eab75fd8a01147e19e26cc9a81205
Christopher Spears <cspears2002@gmail.com>
2025-01-11 15:43:45 -0800

    Read ascii art into a buffer

--- src/main/scala/asciiArt/ASCIIArt.scala
+++ src/main/scala/asciiArt/ASCIIArt.scala
@@ -5,4 +5,11 @@ import scala.collection.mutable.ArrayBuffer
 class ASCIIArt(art: String):
 
   // Need to read the ascii art into this buffer
-  private val myArtBuffer = new ArrayBuffer[String]
+  private val myArtArray = art.split("\n")
+
+  override def toString: String = {
+    myArtArray.mkString("\n")
+  }
+  
+
+
--- src/test/scala/ASCIIArtTest.scala
+++ src/test/scala/ASCIIArtTest.scala
@@ -2,13 +2,36 @@ import asciiArt.ASCIIArt
 import org.scalatest.funsuite.AnyFunSuite
 
 class ASCIIArtTest extends AnyFunSuite:
+  val cat =
+    """
+    /\_/\
+   ( ' ' )
+   (  -  )
+    | | |
+   (__|__)"""
+
+  val helloScalaCoder =
+    """
+      -----
+    / Hello \
+   <  Scala  |
+    \ Coder  /
+      -----
+    """
+
   test("ASCIIArt.toString") {
-    val cat = """
-        /\_/\
-       ( ' ' )
-       (  -  )
-        | | |
-       (__|__)"""
     val myArt = ASCIIArt(cat)
     assert(myArt.toString() == cat)
+  }
+
+  test("ASCIIArt addition") {
+    val coolArt = cat + helloScalaCoder
+    val answer: String =
+      """
+      /\_/\      -----
+     ( ' ' )   / Hello \
+     (  -  )  <  Scala  |
+      | | |    \ Coder  /
+     (__|__)     -----"""
+
   }
