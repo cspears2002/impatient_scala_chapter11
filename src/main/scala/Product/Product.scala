bf702865163fcddd457536e52048351d0c862a1d
Christopher Spears <cspears2002@gmail.com>
2025-03-01 18:25:54 -0800

    Create Product and MyProduct tests

--- src/main/scala/Product/Product.scala
+++ src/main/scala/Product/Product.scala
@@ -1,7 +1,8 @@
 package Product
 
-class Product(n: Int, d: Int)
-
-class MyProduct(n: Int, d: Int) extends Product(n: Int, d: Int):
+class Product(val n: Int, val d: Int):
   def _1: Int = n
   def _2: Int = d
+
+class MyProduct(override val n: Int, override val d: Int) extends Product(n: Int, d: Int):
+  def unapply(input: MyProduct): MyProduct = MyProduct(input._1, input._2)
--- src/test/scala/ProductTest.scala
+++ src/test/scala/ProductTest.scala
@@ -4,9 +4,11 @@ import Product.MyProduct
 import scala.language.postfixOps
 
 class ProductTest extends AnyFunSuite:
-  
+
   test("Unapply") {
-    val myProd = MyProduct(1, 2)
+    val myProd: MyProduct = MyProduct(1, 2)
+    assert(myProd.n == 1)
+    assert(myProd.d == 2)
     assert(myProd._1 == 1)
     assert(myProd._2 == 2)
   }
