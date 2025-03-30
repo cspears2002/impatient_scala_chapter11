8c73b30ddbe8053525a9a28ab4585579c66f3ba4
Christopher Spears <cspears2002@gmail.com>
2025-03-30 15:40:16 -0700

    Extract a sequence from a name

--- src/main/scala/nameWithTitle/NameWithTitle.scala
+++ src/main/scala/nameWithTitle/NameWithTitle.scala
@@ -1,8 +1,13 @@
 package nameWithTitle
 
-import scala.collection.mutable.ArrayBuffer
-import scala.util.matching.Regex
+import nameWithTitle.Title
 
 class NameWithTitle(val name: String)
 
+object NameWithTitle:
+  def apply(name: String) = s"$name"
+
+  def unapplySeq(name: String): Option[Seq[String]] =
+    val nameSeq: Seq[String] = name.split("\\s").toSeq
+    if nameSeq.nonEmpty then Some(nameSeq) else None
       
--- src/test/scala/NameWithTitleTest.scala
+++ src/test/scala/NameWithTitleTest.scala
@@ -2,7 +2,20 @@ import nameWithTitle.{NameWithTitle, Title}
 import org.scalatest.funsuite.AnyFunSuite
 
 class NameWithTitleTest extends AnyFunSuite:
+  val peter: String = "Dr. Peter van der Linden"
+
+  test("Make Name Components") {
+    val petersName: String = NameWithTitle(peter)
+    assert {
+      peter == petersName
+    }
+  }
+
   test("Extract Name Components") {
-    val peter: String = "Dr. Peter van der Linden"
-    val NameWithTitle(title, nameComps) = peter
+    val NameWithTitle(title, firstName, van, der, lastName) = peter
+    assert(title == "Dr.")
+    assert(firstName == "Peter")
+    assert(van == "van")
+    assert(der == "der")
+    assert(lastName == "Linden")
   }
